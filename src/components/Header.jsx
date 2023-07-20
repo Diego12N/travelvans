27713c53df978c2a915a324f9bad5e9c46e583c9
Diego12N <diego.emma.n@outlook.com>
2023-07-19 21:29:03 -0300

    Adding NavLink component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
-import {Link} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export function Header() {
-	return (
-		<header>
-			<Link to="/">TRAVEL'VANS</Link>
-			<nav>
-				<Link to="/host">Host</Link>
-				<Link to="/about">About</Link>
-				<Link to="/vans">Vans List</Link>
-			</nav>
-		</header>
-	);
+  const test = true;
+  return (
+    <header>
+      <Link to="/">TRAVEL'VANS</Link>
+      <nav>
+        <NavLink to="/host" className={({ isActive }) => (isActive ? "active-link" : null)}>
+          Host
+        </NavLink>
+        <NavLink to="/about" className={({ isActive }) => (isActive ? "active-link" : null)}>
+          About
+        </NavLink>
+        <NavLink to="/vans" className={({ isActive }) => (isActive ? "active-link" : null)}>
+          Vans List
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,14 +1,26 @@
-import {Link, Outlet} from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export function HostLayout() {
-	return (
-		<section>
-			<nav className="host-nav">
-				<Link to="/host">Dashboard</Link>
-				<Link to="/host/income">Income</Link>
-				<Link to="/host/reviews">Reviews</Link>
-			</nav>
-			<Outlet></Outlet>
-		</section>
-	);
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
+  return (
+    <section>
+      <nav className="host-nav">
+        <NavLink style={({ isActive }) => (isActive ? activeStyles : null)} to="/host">
+          Dashboard
+        </NavLink>
+        <NavLink style={({ isActive }) => (isActive ? activeStyles : null)} to="/host/income">
+          Income
+        </NavLink>
+        <NavLink style={({ isActive }) => (isActive ? activeStyles : null)} to="/host/reviews">
+          Reviews
+        </NavLink>
+      </nav>
+      <Outlet></Outlet>
+    </section>
+  );
 }
