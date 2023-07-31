c9896e6051ad0aef9e0e4adcc9c79e4549137a51
Diego Emmanuel Naranjo <diego.emma.@outlook.com>
2023-07-31 08:16:21 -0300

    Vans Filter Buttons Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,54 @@
 import "./App.css";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import { AboutPage } from "./pages/AboutPage";
-import { HomePage } from "./pages/HomePage";
-import { VansPage } from "./pages/Vans/VansPage";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {AboutPage} from "./pages/AboutPage";
+import {HomePage} from "./pages/HomePage";
+import {VansPage} from "./pages/Vans/VansPage";
 import "./server.js";
-import { VanDetail } from "./pages/Vans/VanDetail";
-import { Layout } from "./components/Layout";
-import { Dashboard } from "./pages/Host/Dashboard";
-import { Income } from "./pages/Host/Income";
-import { Reviews } from "./pages/Host/Reviews";
-import { HostLayout } from "./components/HostLayout";
-import { HostVans } from "./pages/Host/HostVans";
-import { HostVansDetail } from "./pages/Host/HostVansDetail";
-import { HostVanInfo } from "./pages/Host/HostVanInfo";
-import { HostVanPricing } from "./pages/Host/HostVanPricing";
-import { HostVanPhotos } from "./pages/Host/HostVanPhotos";
+import {VanDetail} from "./pages/Vans/VanDetail";
+import {Layout} from "./components/Layout";
+import {Dashboard} from "./pages/Host/Dashboard";
+import {Income} from "./pages/Host/Income";
+import {Reviews} from "./pages/Host/Reviews";
+import {HostLayout} from "./components/HostLayout";
+import {HostVans} from "./pages/Host/HostVans";
+import {HostVansDetail} from "./pages/Host/HostVansDetail";
+import {HostVanInfo} from "./pages/Host/HostVanInfo";
+import {HostVanPricing} from "./pages/Host/HostVanPricing";
+import {HostVanPhotos} from "./pages/Host/HostVanPhotos";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* Al no definir un path, el componente matchea con todas las rutas, por ende siempre se muestra  */}
-          <Route index element={<HomePage />} />
-          <Route path="about" element={<AboutPage />} />
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					{/* Al no definir un path, el componente matchea con todas las rutas, por ende siempre se muestra  */}
+					<Route index element={<HomePage />} />
+					<Route path="about" element={<AboutPage />} />
 
-          {/* En este caso no necesito un elemente que renderizar ya que las rutas no comparten UI */}
-          <Route path="vans">
-            <Route index element={<VansPage />} />
-            <Route path=":id" element={<VanDetail />} /> {/*Ruta absoluta es /vans/:id*/}
-          </Route>
+					{/* En este caso no necesito un elemente que renderizar ya que las rutas no comparten UI */}
+					<Route path="vans">
+						<Route index element={<VansPage />} />
+						<Route path=":id" element={<VanDetail />} /> {/*Ruta absoluta es /vans/:id*/}
+					</Route>
 
-          <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVansDetail />}>
-              <Route index element={<HostVanInfo />} />
-              <Route path="pricing" element={<HostVanPricing />} />
-              <Route path="photos" element={<HostVanPhotos />} />
-            </Route>
-            <Route path="reviews" element={<Reviews />} />
-            {/* Los path no necesitan la ruta absoluta, ya que en este contexto (Anidadas dentro de Host) 
+					<Route path="host" element={<HostLayout />}>
+						<Route index element={<Dashboard />} />
+						<Route path="income" element={<Income />} />
+						<Route path="vans" element={<HostVans />} />
+						<Route path="vans/:id" element={<HostVansDetail />}>
+							<Route index element={<HostVanInfo />} />
+							<Route path="pricing" element={<HostVanPricing />} />
+							<Route path="photos" element={<HostVanPhotos />} />
+						</Route>
+						<Route path="reviews" element={<Reviews />} />
+						{/* Los path no necesitan la ruta absoluta, ya que en este contexto (Anidadas dentro de Host) 
             no requieren su url completa sino que pueden utilizar la relativa es decir /host/income => income */}
-            {/* Los links que se añadan en los elements podran usar tambien rutas relativas ya que estan dentro de la ruta anidada */}
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+						{/* Los links que se añadan en los elements podran usar tambien rutas relativas ya que estan dentro de la ruta anidada */}
+					</Route>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/pages/Vans/VansPage.jsx
+++ src/pages/Vans/VansPage.jsx
@@ -1,52 +1,55 @@
-import { useEffect, useState } from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {Link, useSearchParams} from "react-router-dom";
 
 export function VansPage() {
-  const [vans, setVans] = useState([]);
-  const [searchParams, setSearchParams] = useSearchParams();
+	const [vans, setVans] = useState([]);
+	const [searchParams, setSearchParams] = useSearchParams();
 
-  const typeFilter = searchParams.get("type");
+	const typeFilter = searchParams.get("type");
 
-  const vansFiltered = typeFilter && typeFilter != "all" ? vans.filter((van) => van.type === typeFilter) : vans;
+	const vansFiltered = typeFilter && typeFilter != "all" ? vans.filter((van) => van.type === typeFilter) : vans;
 
-  const filterVans = (e) => {
-    setSearchParams({
-      type: e.target.value.toLowerCase(),
-    });
-  };
+	const filterVans = (e) => {
+		setSearchParams({
+			type: e.target.value.toLowerCase(),
+		});
+	};
 
-  useEffect(() => {
-    console.log("rerenderizado");
-    fetch("/api/vans")
-      .then((res) => res.json())
-      .then((data) => setVans(data.vans));
-  }, []);
+	useEffect(() => {
+		console.log("rerenderizado");
+		fetch("/api/vans")
+			.then((res) => res.json())
+			.then((data) => setVans(data.vans));
+	}, []);
 
-  const vanItem = vansFiltered.map((van) => {
-    return (
-      <div key={van.id} className="van-tile">
-        <Link to={`/vans/${van.id}`}>
-          <img src={van.imageUrl} alt={van.name} />
-          <div className="van-info">
-            <h3>{van.name}</h3>
-            <p>
-              ${van.price}
-              <span>/day</span>
-            </p>
-          </div>
-          <i className={`van-type ${van.type} selected`}>{van.type}</i>
-        </Link>
-      </div>
-    );
-  });
+	const vanItem = vansFiltered.map((van) => {
+		return (
+			<div key={van.id} className="van-tile">
+				<Link to={`/vans/${van.id}`}>
+					<img src={van.imageUrl} alt={van.name} />
+					<div className="van-info">
+						<h3>{van.name}</h3>
+						<p>
+							${van.price}
+							<span>/day</span>
+						</p>
+					</div>
+					<i className={`van-type ${van.type} selected`}>{van.type}</i>
+				</Link>
+			</div>
+		);
+	});
 
-  return (
-    <div className="van-list-container">
-      <h1>Explore ou van options</h1>
-      <input type="button" value="Simple" onClick={filterVans} />
-      <input type="button" value="Rugged" onClick={filterVans} />
-      <input type="button" value="All" onClick={filterVans} />
-      <div className="van-list">{vanItem}</div>
-    </div>
-  );
+	return (
+		<div className="van-list-container">
+			<h1>Explore ou van options</h1>
+			<div className="vans-list-filter-buttons">
+				<input type="button" value="Simple" onClick={filterVans} className="van-type simple" />
+				<input type="button" value="Rugged" onClick={filterVans} className="van-type rugged" />
+				<input type="button" value="Luxury" onClick={filterVans} className="van-type luxury" />
+				<input type="button" value="All" onClick={filterVans} className="van-type clear-filters" />
+			</div>
+			<div className="van-list">{vanItem}</div>
+		</div>
+	);
 }
